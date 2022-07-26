5dec407b5bd1519d302c268dd1ba0fc940607646
byronekf <byronekf@gmail.com>
2022-07-26 15:22:53 +0100

    Add existing project files to Git

---
+++ data/visited.cjs
@@ -0,0 +1,38 @@
+const { con } = require('../lib/db'); // import for db connection
+const abr2name = require('../public/abr2name.js').abr2name; // dictionary of country abreviations to full names
+
+var visited = {}; // dictionary for abrev to visited status used in setting colours of countries
+
+// function checks if the user has any data for each country in the DB, if not adds new record with default values
+function initialiseVisits(userID) {
+  Object.keys(abr2name).forEach(element => {
+    con.query(`SELECT * FROM visited_status WHERE userID = ${userID} and countryAbr = '${element}'`, (error, rows) => {
+      if (rows.length == 0) {
+        const row = { userID: userID, countryAbr: element, countryName: abr2name[element], visited: 0 };
+        con.query('INSERT INTO visited_status SET ?', row, (err, res) => {
+          if (err) throw err;
+          console.log('Last insert ID: ', res.insertId);
+        });
+      }
+    });
+  });
+}
+
+// function to retrieve visited status of all countries
+function getVisited(userID) {
+  return new Promise(function (resolve, reject) {
+    con.query(`SELECT countryAbr, visited FROM visited_status WHERE userID = ${userID}`, (error, rows) => {    
+      rows.forEach(row => {
+        visited[row.countryAbr] = Boolean(row.visited);
+      });
+      resolve(visited);
+    });
+  });
+}
+
+// function to update visited status
+function updateVisitedStatus(userID, countryAbr, status) {
+  con.query(`UPDATE visited_status SET visited = ${status} WHERE userID = ${userID} AND countryAbr = '${countryAbr}'`);
+}
+
+module.exports = { initialiseVisits, getVisited, updateVisitedStatus }; // exports all functions for use in other files
