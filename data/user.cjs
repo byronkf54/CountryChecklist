a2bfd5472febf417a06e7494bca02a85e8ff9ced
byronekf <byronekf@gmail.com>
2022-10-03 18:34:25 +0100

    Added basic login/register functionality

--- data/user.cjs
+++ data/user.cjs
@@ -1,1 +1,42 @@
-const pool = require('../lib/db'); // import for db connection
+const { Result } = require('express-validator');
+const models = require('../lib/models');
+
+const pool = require('../lib/db').pool; // import for db connection
+
+function createUser(user, hashedPassword) {
+    return new Promise(function (resolve, reject) {
+        // check if username already exists
+        pool.query(`SELECT * FROM users WHERE user = '${user}'`, (err, rows) => {
+            if (err) throw (err)
+
+            if (rows.length != 0) {
+                resolve(-1);
+            }
+            else {
+                pool.query(`INSERT INTO users (user, password) VALUES ('${user}', '${hashedPassword}')`, (err, result) => {
+                    if (err) throw (err)
+                    resolve(1);
+                })
+            }
+        })
+    });
+}
+
+function getUser(user) {
+    return new Promise(function (resolve, reject) {
+        // select user by name
+        pool.query(`SELECT * FROM users WHERE user = '${user}'`, (err, rows) => {
+            if (err) throw (err)
+
+            if (rows.length == 0) {
+                resolve(models.User.build());
+            }
+            else {
+                resolve(rows[0]);
+            }
+        });
+    });
+}
+
+
+module.exports = { createUser, getUser };
--- index.cjs
+++ index.cjs
@@ -1,12 +1,16 @@
 var express = require("express");
 const bcrypt = require("bcrypt");
 var port = process.env.PORT || 3000;
-var bodyParser=require("body-parser");
+var bodyParser = require("body-parser");
 var fs = require('fs');
 
+// custom files we require access to
 var abr2name = require("./public/abr2name.js").abr2name;
 var visited_db = require("./data/visited.cjs");
 var user_db = require("./data/user.cjs");
+const generateAccessToken = require("./lib/generateAccessToken");
+const secretJWTKey = process.env.secretJWTKey;
+const { default: e } = require("express");
 
 var app = express();
 app.use(bodyParser.json());
@@ -18,7 +22,7 @@ app.use(bodyParser.urlencoded({
 app.set('view engine', 'ejs');
 
 app.post('/CountryList', function(req,res) {
-    var userID = 1;
+    var userID = req.body.userID;
     visited_db.getVisited(userID).then((visited) => {
         return res.render('CountryList', { visited: visited, abr2name: abr2name });
     });
@@ -26,7 +30,7 @@ app.post('/CountryList', function(req,res) {
 
 app.post('/CountryMap', function(req,res) {
     // check if user has data in DB
-    var userID = 1;
+    var userID = req.body.userID;
     visited_db.initialiseVisits(userID);
     // get current visit status
     visited_db.getVisited(userID).then((visited) => {
@@ -39,7 +43,7 @@ app.get('/abr2name', function(req, res) {
 })
 
 app.get('/getVisitedCount', function(req, res) {
-    var userID = 1;
+    var userID = req.body.userID;
     visited_db.getVisitedCount(userID).then((count) => {
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify(count));
@@ -49,35 +53,94 @@ app.get('/getVisitedCount', function(req, res) {
 app.post('/updateVisitedStatus', function(req, res) {
     var countryAbr = req.body.countryAbr;
     var status = req.body.visitedStatus;
-    var userID = 1;
+    var userID = req.body.userID;
     //update tempDB
     visited_db.updateVisitedStatus(userID, countryAbr, status);
 })
 
-app.post('/createUser', async (req, res) => {
-    const user = req.body.name;
+app.post('/createUser', async function(req, res) {
+    if (req.body.user == undefined) {
+        res.render('register');
+    }
+    const user = req.body.user;
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    // user_db.
+    user_db.createUser(user, hashedPassword).then((result) => {
+        if (result == -1) {
+            console.log("username exists");
+            return res.render('register', { errors: ["Username is not available"] })
+        }
+        else {
+            // generate access token so user can stay logged in
+            const token = generateAccessToken({user: user});
+            console.log("token: " + token);
+            res.json({accessToken: token});
+
+            // check if user has data in DB
+            const userID = result.userID;
+            visited_db.initialiseVisits(userID);
+            // get current visit status
+            visited_db.getVisited(userID).then((visited) => {
+                return res.render('home', { visited: visited, abr2name: abr2name });
+            });
+        }
+    });    
+})
+
+app.post('/login', function(req, res) {
+    const user = req.body.name;
+    const password = req.body.password;
+
+    console.log("User: " + user);
+
+    user_db.getUser(user).then((result) => {
+        console.log("Get User Result: " + result.user);
+        if (result.user == undefined) res.sendStatus(404); // user does not exist
+        else {
+            const hashedPassword = result.password;
+            bcrypt.compare(password, hashedPassword).then((comparison) => {
+                if (comparison) {
+                    // generate access token so user can stay logged in
+                    const token = generateAccessToken({user: user});
+                    console.log("token: " + token);
+                    res.json({accessToken: token});
+
+                    // check if user has data in DB
+                    const userID = result.userID;
+                    visited_db.initialiseVisits(userID);
+                    // get current visit status
+                    visited_db.getVisited(userID).then((visited) => {
+                        return res.render('home', { visited: visited, abr2name: abr2name });
+                    });
+                }
+                else { // password incorrect
+                    console.log("Retry password");
+                    return res.render()
+                }
+            })
+        }
+    })
 })
 
 app.get('/home', function(req,res) {
     
 })
 
+app.get('/register', function(req, res) {
+    res.render('register', { errors: "" });
+})
+
+app.get('/login', function(req, res) {
+    res.render('login');
+})
+
 app.get('/',function(req,res) {
     res.set({
         'Access-control-Allow-Origin': '*'
     });
-    // check user is logged in
-    // check if user has data in DB
-    var userID = 1;
-    visited_db.initialiseVisits(userID);
-    // get current visit status
-    visited_db.getVisited(userID).then((visited) => {
-        // return res.render('home.ejs');
-        return res.render('home', { visited: visited, abr2name: abr2name });
-    });
-    
+    // check user is logged in    
+    if (req.body.accessToken == undefined) {
+        res.render('login');
+    }
 }).listen(port)
   
   
