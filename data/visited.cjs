bb02c7a309f0ff075b55548b6ba9ab86d990d8d7
byronekf <byronekf@gmail.com>
2023-05-09 14:45:32 +0100

    Migrating to MongoDB - first steps

--- data/visited.cjs
+++ data/visited.cjs
@@ -1,46 +1,43 @@
-const pool = require('../lib/db').pool; // import for db connection
+const db = require('../lib/db').db; // import for db connection
 const abr2name = require('../public/abr2name.js').abr2name; // dictionary of country abreviations to full names
 
 var visited = {}; // dictionary for abrev to visited status used in setting colours of countries
 
 // function checks if the user has any data for each country in the DB, if not adds new record with default values
-function initialiseVisits(userID) {
-  Object.keys(abr2name).forEach(element => {
-    pool.query(`SELECT * FROM visited_status WHERE userID = ${userID} and countryAbr = '${element}'`, (err, rows) => {
-      if (rows.length == 0) {
-        const row = { userID: userID, countryAbr: element, countryName: abr2name[element], visited: 0 };
-        pool.query('INSERT INTO visited_status SET ?', row, (err, res) => {
-          if (err) throw err;
-          console.log('Last insert ID: ', res.insertId);
-        });
-      }
-    });
-  });
+async function initialiseVisits(userID) {
+    for (const element of Object.keys(abr2name)) {
+        const existingRow = await db.visited_status.findOne({ userID: userID, countryAbr: element });
+        if (!existingRow) {
+            const row = { userID: userID, countryAbr: element, countryName: abr2name[element], visited: 0 };
+            const result = await db.visited_status.insertOne(row);
+            console.log(`New document inserted with _id: ${result.insertedId}`);
+        }
+    }
 }
 
 // function to retrieve visited status of all countries
 function getVisited(userID) {
-  return new Promise(function (resolve, reject) {
-    pool.query(`SELECT countryAbr, visited FROM visited_status WHERE userID = ${userID}`, (err, rows) => {
-      rows.forEach(row => {
-        visited[row.countryAbr] = Boolean(row.visited);
-      });
-      resolve(visited);
+    return new Promise(function (resolve, reject) {
+        pool.query(`SELECT countryAbr, visited FROM visited_status WHERE userID = ${userID}`, (err, rows) => {
+            rows.forEach(row => {
+                visited[row.countryAbr] = Boolean(row.visited);
+            });
+            resolve(visited);
+        });
     });
-  });
 }
 
 function getVisitedCount(userID) {
-  return new Promise(function (resolve, reject) {
-    pool.query(`SELECT COUNT(*) AS visitedCount FROM visited_status WHERE userID = ${userID} AND visited = 1`, (err, rows) => {
-      resolve(rows[0].visitedCount);
+    return new Promise(function (resolve, reject) {
+        pool.query(`SELECT COUNT(*) AS visitedCount FROM visited_status WHERE userID = ${userID} AND visited = 1`, (err, rows) => {
+            resolve(rows[0].visitedCount);
+        });
     });
-  });
 }
 
 // function to update visited status
 function updateVisitedStatus(userID, countryAbr, status) {
-  pool.query(`UPDATE visited_status SET visited = ${status} WHERE userID = ${userID} AND countryAbr = '${countryAbr}'`);
+    pool.query(`UPDATE visited_status SET visited = ${status} WHERE userID = ${userID} AND countryAbr = '${countryAbr}'`);
 }
 
-module.exports = { initialiseVisits, getVisited, getVisitedCount, updateVisitedStatus }; // exports all functions for use in other files
+module.exports = {initialiseVisits, getVisited, getVisitedCount, updateVisitedStatus}; // exports all functions for use in other files
